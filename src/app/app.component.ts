6ee9cefb341b9eb91240844d348dd228d739f1ad
Jason Dunn <jason.dunn@vehicletracking.com>
2021-05-17 07:38:13 -0400

    adding loopback/filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { whereBuilder } from '@loopback/filter';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'polyfill-example';
+  where: whereBuilder = new whereBuilder();
 }
